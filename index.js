25ee70716de5303e288bde3fd5a831f56a8a740e
Zamzam <Zamzamhassan44@gmail.com>
2022-06-08 09:01:31 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+
+function saturdayFun (action = "roller-skate") {
+  return `This Saturday, I want to ${action}!`; 
+  }
+  saturdayFun();
+  saturdayFun("bathe my dog");
+
+
+function mondayWork (work = "go to the office"){
+  return `This Monday, I will ${work}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(test = "*"){ 
+  return function (myCompliment = "special"){
+    return `You are ${test}${myCompliment}${test}!`;
+  }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
